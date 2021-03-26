9fda63082d74333c0109b6e021a55eae506c9c31
Prabitha Krishnaprakash <prabitha.krishnaprakash@qrtize.app>
2021-03-26 15:31:39 +0530

    SINGLE OBSERVABLE :: Implemented Single Observable

--- app/src/main/java/com/example/weatherrepository/retrofit/APIService.kt
+++ app/src/main/java/com/example/weatherrepository/retrofit/APIService.kt
@@ -2,10 +2,14 @@ package com.example.weatherrepository.retrofit
 
 import com.example.weatherrepository.model.WeatherResponse
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface APIService {
     @GET("current.json?key=1a2028b00b8e4099844143106212503&q=Coimbatore&aqi=no")
     fun fetchWeatherReport( ):Observable<WeatherResponse>
+
+    @GET("current.json?key=1a2028b00b8e4099844143106212503&q=Delhi&aqi=no")
+    fun fetchWeatherReportSingle( ): Single<WeatherResponse>
 }
--- app/src/main/java/com/example/weatherrepository/viewmodels/WeatherFragmentViewModel.kt
+++ app/src/main/java/com/example/weatherrepository/viewmodels/WeatherFragmentViewModel.kt
@@ -1,43 +1,67 @@
 package com.example.weatherrepository.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weatherrepository.model.WeatherResponse
 import com.example.weatherrepository.retrofit.APIService
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.core.SingleObserver
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 class WeatherFragmentViewModel @Inject constructor(
-    private val apiService: APIService
+        private val apiService: APIService
 ) : ViewModel(
 ) {
-    var weatherValue = MutableLiveData<String>("")
-
+    var weatherValue = MutableLiveData<String>("Fetching")
+    var weatherValueSingle = MutableLiveData<String>("Fetching")
     fun fetchWeather() {
         apiService.fetchWeatherReport().subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread()).subscribe(
-                object : Observer<WeatherResponse> {
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(
+                        object : Observer<WeatherResponse> {
+                            override fun onSubscribe(d: Disposable?) {
+                                print(d.toString())
+                            }
+
+                            override fun onNext(t: WeatherResponse?) {
+                                weatherValue.postValue("The temperature at Coimbatore is ${t!!.current.feelslike_c.toString()}")
+                            }
+
+                            override fun onError(e: Throwable?) {
+                                print(e?.localizedMessage)
+                            }
+
+                            override fun onComplete() {
+                                print("COmpleted")
+                            }
+
+
+                        }
+                )
+    }
+
+    fun fetchWeatherSingle() {
+
+
+        apiService.fetchWeatherReportSingle().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : SingleObserver<WeatherResponse> {
                     override fun onSubscribe(d: Disposable?) {
-                        print(d.toString())
+
+                        Log.d("d", d.toString())
                     }
 
-                    override fun onNext(t: WeatherResponse?) {
-                        weatherValue.postValue("The temperature at Coimbatore is ${t!!.current.feelslike_c.toString()}")
+                    override fun onSuccess(t: WeatherResponse?) {
+                        weatherValueSingle.postValue("The temperature at Delhi is ${t!!.current.feelslike_c}")
                     }
 
                     override fun onError(e: Throwable?) {
-                        print(e?.localizedMessage)
+                        Log.d("error", e?.localizedMessage.toString())
                     }
 
-                    override fun onComplete() {
-                        print("COmpleted")
-                    }
-
-
-                }
-            )
+                })
     }
 }
